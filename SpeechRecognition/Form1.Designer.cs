fd6d31627f0f9b572e81164b4540581b9297f05f
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-08-18 09:38:21 +0100

    Code convert Text to Audio

---
+++ SpeechRecognition/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace SpeechRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtVoice = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTextToAudio = new System.Windows.Forms.Button();
+            this.barVolume = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.barSpeed = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.barVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtVoice
+            // 
+            this.txtVoice.Location = new System.Drawing.Point(24, 80);
+            this.txtVoice.Multiline = true;
+            this.txtVoice.Name = "txtVoice";
+            this.txtVoice.Size = new System.Drawing.Size(786, 347);
+            this.txtVoice.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(24, 456);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 52);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(165, 456);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(128, 52);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(33, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "For Evana, Joana and Meera";
+            // 
+            // btnTextToAudio
+            // 
+            this.btnTextToAudio.Location = new System.Drawing.Point(689, 456);
+            this.btnTextToAudio.Name = "btnTextToAudio";
+            this.btnTextToAudio.Size = new System.Drawing.Size(121, 52);
+            this.btnTextToAudio.TabIndex = 4;
+            this.btnTextToAudio.Text = "Text to Audio";
+            this.btnTextToAudio.UseVisualStyleBackColor = true;
+            this.btnTextToAudio.Click += new System.EventHandler(this.btnTextToAudio_Click);
+            // 
+            // barVolume
+            // 
+            this.barVolume.Location = new System.Drawing.Point(92, 524);
+            this.barVolume.Name = "barVolume";
+            this.barVolume.Size = new System.Drawing.Size(718, 45);
+            this.barVolume.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 524);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Volume";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 584);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Speed";
+            // 
+            // barSpeed
+            // 
+            this.barSpeed.Location = new System.Drawing.Point(92, 584);
+            this.barSpeed.Minimum = -10;
+            this.barSpeed.Name = "barSpeed";
+            this.barSpeed.Size = new System.Drawing.Size(718, 45);
+            this.barSpeed.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 676);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.barSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.barVolume);
+            this.Controls.Add(this.btnTextToAudio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtVoice);
+            this.Name = "Form1";
+            this.Text = "Speech recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVoice;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTextToAudio;
+        private System.Windows.Forms.TrackBar barVolume;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar barSpeed;
+    }
+}
+
---
+++ SpeechRecognition/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+ 
+
+
+namespace SpeechRecognition
+{
+    public partial class Form1 : Form
+    {
+
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        public   Choices clist = new Choices();
+
+
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            barVolume.Value = 10;
+            barSpeed.Value = 0;
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+
+
+
+            clist.Add(new string[]
+                {"Hello","How are you", "what is the current time", "Open Chrome", "Thank you", "Close"}
+                );
+
+
+
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_speachrecognised;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+
+            
+
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+
+
+
+
+        }
+
+        private void sre_speachrecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            //  "Hello","How are you", "what is the current time", "Open Chrome", "Thank you", "Close"
+            try
+            {
+
+                switch (e.Result.Text.ToString())
+            {
+
+                case "Hello":
+                    ss.SpeakAsync("Hello How are you? How can i help you?");
+
+                    break;
+
+                case "How are you":
+                    ss.SpeakAsync("I am fine. Thank you.How are you? How can i help you?");
+
+                    break;
+
+
+                case "what is the current time":
+                    ss.SpeakAsync("Current time is. " + DateTime.Now.ToLongTimeString()) ;
+
+                    break;
+
+                case "Thank you":
+                    ss.SpeakAsync("You are welcome. Can i help you further?");
+
+                    break;
+
+                case "Open Chrome":
+                    ss.SpeakAsync("Trying to open  chrome");
+                    Process.Start("Chrome", "https://www.google.co.uk");
+
+                    break;
+
+
+                case "Close":
+                    ss.SpeakAsync("Thank you. Catch you later. Bye.");
+
+                    Application.Exit();
+
+                    break;
+
+                    default:
+
+                 ss.SpeakAsync("Please explain it in detail.");
+                      
+
+                        break;
+
+
+                }
+
+
+
+            txtVoice.Text += e.Result.Text.ToString() + Environment.NewLine;
+
+
+
+            
+
+            }
+                   
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+
+
+
+}
+
+
+       
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sre.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+
+        }
+
+        private void btnTextToAudio_Click(object sender, EventArgs e)
+        {
+
+
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            synthesizer.Volume =barVolume.Value;
+            synthesizer.Rate = barSpeed.Value  ;// -2;     
+ 
+
+ 
+            synthesizer.SpeakAsync(txtVoice.Text.Trim());
+
+        }
+    }
+}
